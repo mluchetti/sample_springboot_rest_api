fa2a0381ec240660cb6571c70e4477b7b5e99256
mluchetti <mbluchetti@gmail.com>
2017-10-26 22:17:38 -0500

    project configuration

--- src/main/java/com/luchetti/springboot/firstApplication/model/PersonWrapper.java
+++ src/main/java/com/luchetti/springboot/firstApplication/model/PersonWrapper.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
